58c80b119f20ed18e34650d613395459945ee311
Kacper Doga <doga.kacper@gmail.com>
2024-01-08 01:00:23 +0100

    limit movement on floors to its borders

--- Movable.hpp
+++ Movable.hpp
@@ -20,23 +20,29 @@ extern "C" {
 class Movable : public GameObject {
 using GameObject::GameObject;
 public:
-	void move(int direction, double size, GameObject block) {
+	void move(int direction, double distance, GameObject block) {
 		switch (direction) {
 			case SDLK_LEFT:
-				this->setPositionX(this->getBeginningAxisX() - size);
+				if (block.getBeginningAxisX() > (int)(this->getBeginningAxisX() - distance))
+					this->setPositionX(block.getBeginningAxisX());
+				else
+					this->setPositionX(this->getBeginningAxisX() - distance);
 				break;
 			case SDLK_RIGHT:
-				this->setPositionX(this->getBeginningAxisX() + size);
+				if (block.getEndAxisX() < this->getEndAxisX() + distance)
+					this->setPositionX(block.getEndAxisX() - this->width);
+				else
+					this->setPositionX(this->getBeginningAxisX() + distance);
 				break;
 			case SDLK_UP:
-				if (this->getBeginningAxisY() - size >= block.getBeginningAxisY() - this->height)
-					this->setPositionY(this->getBeginningAxisY() - size);
+				if (this->getBeginningAxisY() - distance > block.getBeginningAxisY() - this->height)
+					this->setPositionY(this->getBeginningAxisY() - distance);
 				else
 					this->setPositionY(block.getBeginningAxisY() - this->height);
 				break;
 			case SDLK_DOWN:
-				if (this->getEndAxisY() + size < block.getEndAxisY())
-					this->setPositionY(this->getBeginningAxisY() + size);
+				if (this->getEndAxisY() + distance < block.getEndAxisY())
+					this->setPositionY(this->getBeginningAxisY() + distance);
 				else
 					this->setPositionY(block.getEndAxisY() - this->height);
 				break;
