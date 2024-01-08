22d9dbd30902309a4d0162cd2a63dc5afda591db
Kacper Doga <doga.kacper@gmail.com>
2024-01-08 23:04:36 +0100

    create const values for game view position

--- DEFINED_VALUES.h
+++ DEFINED_VALUES.h
@@ -1,8 +1,22 @@
+// screen size in pixels 
 #define SCREEN_WIDTH	640
 #define SCREEN_HEIGHT	480
 
-#define MAX_LADDERS 8
-#define MAX_FLOORS 4
+// border & game items size
+#define BORDER_SIZE 16
+#define INFO_BAR_SIZE 48
+#define FLOOR_HEIGHT 112
 
+// game view position
+#define GAME_BEG_X BORDER_SIZE
+#define GAME_BEG_Y INFO_BAR_SIZE
+#define GAME_END_X SCREEN_WIDTH - BORDER_SIZE
+#define GAME_END_Y SCREEN_HEIGHT - BORDER_SIZE
+
+// 
+#define MAX_LADDERS 8
+#define MAX_FLOORS 8
+
+// values used as parameter to switch between modes
 #define FLOOR 1
 #define LADDER 2
